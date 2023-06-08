e2f00fee0e5173ab1f22d504306c469d4484e783
Ray7788 <87214670+Ray7788@users.noreply.github.com>
2023-06-08 10:07:18 +0100

    1

---
+++ gcd.py
@@ -0,0 +1,21 @@
+x = int(input("Input a number1: \n"))
+y = int(input("Input a number2: \n"))
+
+''' 
+The greatest common divisor, gcd, or highest
+common factor, hcf, of two numbers is largest
+integer that divides them both.
+
+• If gcd(a, b) = 1(or hcf(a, b) = 1) we say that a and b are relatively prime.
+ 
+'''
+def gcd(a,b):
+    if b == 0:
+        if a == 1:
+            print("iff", a, "has a multiplicative inverse in Zn")
+        return a       
+    else:
+        r = a % b
+        return gcd(b,r)
+    
+print(gcd(x,y))
